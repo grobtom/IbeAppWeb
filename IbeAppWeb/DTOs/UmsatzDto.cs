b1f296b6ab7543f243da3e9b02410c2747667d6b
grobtom <grobbauer@gt-informatik.de>
2025-05-12 22:08:01 +0200

    refactored umsatz

--- IbeAppWeb/DTOs/UmsatzDto.cs
+++ IbeAppWeb/DTOs/UmsatzDto.cs
@@ -5,6 +5,7 @@ namespace IbeAppWeb.DTOs
 {
     public class UmsatzDto
     {
+        public string PkLvPos { get; set; } = string.Empty;
         public DateTime SaniertAm { get; set; }
         public string Fahrzeug { get; set; } = string.Empty;
         public string Kolonnenfuehrer { get; set; } = string.Empty;
