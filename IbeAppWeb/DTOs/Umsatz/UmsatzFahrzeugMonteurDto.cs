96895972f762146590c3e7db8cf32e658fcf56ff
grobtom <grobbauer@gt-informatik.de>
2025-05-14 08:51:47 +0200

    added kurtext abd lvpos to umsatz

--- IbeAppWeb/DTOs/Umsatz/UmsatzFahrzeugMonteurDto.cs
+++ IbeAppWeb/DTOs/Umsatz/UmsatzFahrzeugMonteurDto.cs
@@ -4,6 +4,7 @@ namespace IbeAppWeb.DTOs.Umsatz;
 public class UmsatzFahrzeugMonteurDto
 {
     public string PkLvPos { get; init; } = string.Empty;
+    public string Kurztext { get; init; } = string.Empty;
     public string ProjectName { get; init; }
     public DateTime Ausfuehrungsdatum { get; init; }
     public string Fahrzeug { get; init; }
