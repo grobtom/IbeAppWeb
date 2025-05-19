30bd523a1708a072a9989043fadfebea51090929
grobtom <grobbauer@gt-informatik.de>
2025-05-19 13:36:15 +0200

    added Totalumsatz to Umsatzall and refactored Arbeitsscheine to work taks in parallel

--- IbeAppWeb/DTOs/Umsatz/UmsatzFahrzeugMonteurProjectDto.cs
+++ IbeAppWeb/DTOs/Umsatz/UmsatzFahrzeugMonteurProjectDto.cs
@@ -5,4 +5,5 @@ public class UmsatzFahrzeugMonteurProjectDto
 {
     public Project Project { get; set; } = new();
     public List<UmsatzFahrzeugMonteurDto> UmsatzProject { get; set; } = new();
+    public decimal TotalUmsatz { get; set; }
 }
